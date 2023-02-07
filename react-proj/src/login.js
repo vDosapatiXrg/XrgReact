7ad46ef43e0ce13883afeaa77650cac819963f4c
vDosapatiXrg <124669822+vDosapatiXrg@users.noreply.github.com>
2023-02-07 15:28:48 +0530

    login
    .

---
+++ react-proj/src/login.js
@@ -0,0 +1,57 @@
+import "./Login.css";
+import { Form, Button, Input } from "antd";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    navigate("/home");
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <div className="illustration-wrapper"></div>
+        <Form
+          name="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <p className="form-title">New Relic</p>
+          <p>Login to the Dashboard</p>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              LOGIN
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
